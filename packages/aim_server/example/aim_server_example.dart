5a866be51c7d533eaaea08b7dd577800209635aa
dosukoi-android <dosukoroid@gmail.com>
2025-12-24 09:59:50 +0900

    create aim_server package

---
+++ packages/aim_server/example/aim_server_example.dart
@@ -0,0 +1,6 @@
+import 'package:aim_server/aim_server.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/aim_server/test/aim_server_test.dart
@@ -0,0 +1,16 @@
+import 'package:aim_server/aim_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
