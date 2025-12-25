bb593255684a372fc1e47967b88e517f16d7414a
dosukoi-android <dosukoroid@gmail.com>
2025-12-26 02:54:55 +0900

    fix

--- packages/aim_server_multipart/example/main.dart
+++ packages/aim_server_multipart/example/main.dart
@@ -6,7 +6,7 @@ import 'package:aim_server_multipart/src/multipart_request.dart';
 void main() {
   final app = Aim();
   app.post('/upload', (c) async {
-    final data = c.req.multipart();
+    c.req.multipart();
     return c.text('received');
   });
 
@@ -14,9 +14,4 @@ void main() {
     host: InternetAddress.anyIPv4,
     port: 8080,
   );
-}
-
-void hoge () {
-  final client= HttpClient();
-
 }
