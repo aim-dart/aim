c0396c7660d7aea595f15a4b6d6a16b2038829f9
dosukoi-android <dosukoroid@gmail.com>
2026-01-22 09:36:32 +0900

    fix warning

--- examples/orm-sample/lib/test.dart
+++ examples/orm-sample/lib/test.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:aim_orm/aim_orm.dart';
 import 'package:aim_orm_postgres/aim_orm_postgres.dart';
 import 'package:aim_postgres/aim_postgres.dart';
-import 'package:uuid/uuid.dart';
 
 part 'test.g.dart';
 
